c8388c02b60eac6f2fd334508acbf99824481ed9
Jake Walker <hi@jakew.me>
2018-12-05 22:20:50 +0000

    Increased MONEY_SUPPLY

--- src/config/CryptoNoteConfig.h
+++ src/config/CryptoNoteConfig.h
@@ -31,8 +31,8 @@ const size_t   BLOCKCHAIN_TIMESTAMP_CHECK_WINDOW             = 60;
 const size_t   BLOCKCHAIN_TIMESTAMP_CHECK_WINDOW_V3          = 11;
 
 // MONEY_SUPPLY - total number coins to be generated
-// (10^2 * 10,000) 10,000.00 coins
-const uint64_t MONEY_SUPPLY                                  = UINT64_C(1000000);
+// (10^2 * 1000000000000) 1000000000000 coins
+const uint64_t MONEY_SUPPLY                                  = UINT64_C(100000000000000);
 const uint32_t ZAWY_DIFFICULTY_BLOCK_INDEX                   = 0;
 const size_t   ZAWY_DIFFICULTY_V2                            = 0;
 const uint8_t  ZAWY_DIFFICULTY_DIFFICULTY_BLOCK_VERSION      = 3;
